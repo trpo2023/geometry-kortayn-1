9f93f8c9051428f72ad6030f6693675c7f13fd7a
Yuri Katokhin <kortayn@users.noreply.github.com>
2023-03-02 22:16:35 +0700

    Create output

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 
 int main()
 {
@@ -11,7 +12,14 @@ int main()
         return 1;
     }
 
-    int length = 0, element = 0;
+    int open_bracket = 0,
+        close_bracket = 0,
+        first_number = 0,
+        second_number = 0;
+
+    int length = 0,
+        element = 0, 
+        error = 0;
 
     while(1)
     {
@@ -20,7 +28,127 @@ int main()
         length++;
     }
 
-    printf("%d", length);
+    char a[length], b[6] = "circle";
+    file = fopen("test.txt", "r");
+
+    while(fgets(a, length + 1, file))
+    {
+        error = 0;
+        printf("%s", a);
+
+        for(int i = 0; i < length; i++)
+        {
+            if(a[i] == '(')
+            {
+                open_bracket = i;
+                break;
+            }
+        }
+
+        for(int i = 0; i < length; i++)
+        {
+            if(a[i] == ')')
+            {
+                close_bracket = i;
+                break;
+            }
+        }
+
+        for(int i = open_bracket + 1; i < length; i++)
+        {
+            if(a[i] == '(' && open_bracket != 0)
+            {
+                close_bracket = i;
+
+                for(int j = 0; j < i; j++) printf(" ");
+                printf("^\n");
+                printf("Error at column %d: expected ')'\n", i);
+
+                error = 1;;
+                break;
+            }
+        }
+
+        for(int i = open_bracket + 1; i < length; i++)
+        {
+            if(a[i] == ' ')
+            {
+                first_number = i;
+                break;
+            }
+        }
+
+        for(int i = first_number + 1; i < length; i++)
+        {
+            if(a[i] == ',')
+            {
+                second_number = i;
+                break;
+            }
+        }
+
+        for(int i = 0; i < open_bracket; i++)
+        {
+            if(a[i] != b[i])
+            {
+                printf("^\nError at column 0: expected 'circle'\n");
+
+                error = 1;
+                break;
+            }
+        }
+
+        for(int i = open_bracket + 1; i < first_number; i++)
+        {
+            if(isdigit(a[i]) == 0 && a[i] != '.')
+            {
+                for(int j = 0; j < i; j++) printf(" ");
+                printf("^\n");
+                printf("Error at column %d: expected '<double>'\n", i);
+
+                error = 1;
+                break;
+            }
+        }
+
+        for(int i = first_number + 1; i < second_number; i++)
+        {
+            if(isdigit(a[i]) == 0 && a[i] != '.')
+            {
+                for(int j = 0; j < i; j++) printf(" ");
+                printf("^\n");
+                printf("Error at column %d: expected '<double>'\n", i);
+
+                error = 1;
+                break;
+            }
+        }
+
+        for(int i = second_number + 2; i < close_bracket; i++)
+        {
+            if(isdigit(a[i]) == 0 && a[i] != '.')
+            {
+                for(int j = 0; j < i; j++) printf(" ");
+                printf("^\n");
+                printf("Error at column %d: expected '<double>'\n", i);
+
+                error = 1;
+                break;
+            }
+        }
+
+        if(a[close_bracket + 1] != '\n' && a[close_bracket + 1] != EOF)
+        {
+            printf("\n");
+            for(int j = 0; j < close_bracket + 1; j++) printf(" ");
+            printf("^\n");
+            printf("Error at column %d: unexpected tokens\n", close_bracket + 1);
+            error = 1;
+        }
+
+        if(error == 0) printf("No Errors\n");
+        printf("\n");
+    }
 
     return 0;
 }
