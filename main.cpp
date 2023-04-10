647f7a6ce85a601f97bac5f4a468495001210ce2
Yuri Katokhin <kortayn@users.noreply.github.com>
2023-04-10 09:51:15 +0700

    Merge figure check fucntions into one

--- main.cpp
+++ main.cpp
@@ -96,28 +96,7 @@ int *CommaCount(string line, int &comma_count, int open_bracket, int close_brack
     return commas;
 }
 
-void CircleCheck(string line, int *commas, int begin, int end, int error)
-{
-    int space;
-
-    for(int i = begin + 1; i < commas[0]; i++)
-        if(line[i] == ' ') space = i;
-
-    error += NumberCheck(line, begin + 1, space);
-    error += NumberCheck(line, space + 1, commas[0]);
-    error += NumberCheck(line, commas[0] + 2, end);
-
-    if(!error)
-    {
-        float radius = stod(line.substr(commas[0] + 2, end));
-        cout << "\nSquare = " << pi * pow(radius, 2)
-             << "\nPerimeter = " << pi * 2 * radius << "\n";
-    }
-
-    delete commas;
-}
-
-void TriangleCheck(string line, int *commas, int comma_count, int begin, int end, int error)
+void FigureCheck(string line, int *commas, int comma_count, int begin, int end, int error)
 {
     int space;
 
@@ -141,80 +120,73 @@ void TriangleCheck(string line, int *commas, int comma_count, int begin, int end
         begin = commas[i] + 1;
     }
 
-    for(int i = commas[comma_count - 1] + 2; i < end; i++)
-        if(line[i] == ' ') space = i;
-
-    error += NumberCheck(line, commas[comma_count - 1] + 2, space);
-    error += NumberCheck(line, space + 1, end);
-
-    if(!error)
+    if(line.substr(0, 6) == "circle")
     {
-        xy[comma_count][0] = stod(line.substr(commas[comma_count - 1] + 2, space));
-        xy[comma_count][1] = stod(line.substr(space + 1, end));
-
-        double a = sqrt(pow((xy[1][0] - xy[0][0]), 2) + pow((xy[1][1] - xy[0][1]), 2));
-        double b = sqrt(pow((xy[2][0] - xy[1][0]), 2) + pow((xy[2][1] - xy[1][1]), 2));
-        double c = sqrt(pow((xy[0][0] - xy[2][0]), 2) + pow((xy[0][1] - xy[2][1]), 2));
-
-        double p = a + b + c;
-
-        cout << "\nSquare = " << sqrt(p / 2 * (p / 2 - a) * (p / 2 - b) * (p / 2 - c))
-             << "\nPerimeter = " << p << "\n";
-    }
-
-    delete commas;
-}
-
-void PolygonCheck(string line, int *commas, int comma_count, int begin, int end, int error)
-{
-    int space;
-
-    double **xy = new double*[comma_count + 1];
-    for(int i = 0; i < comma_count + 1; i++) xy[i] = new double[2];
-
-    for(int i = 0; i < comma_count; i++)
-    {
-        for(int j = begin + 1; j < commas[i]; j++)
-            if(line[j] == ' ') space = j;
-
-        error += NumberCheck(line, begin + 1, space);
-        error += NumberCheck(line, space + 1, commas[i]);
+        error += NumberCheck(line, commas[0] + 2, end);
 
         if(!error)
         {
-            xy[i][0] = stod(line.substr(begin + 1, space));
-            xy[i][1] = stod(line.substr(space + 1, commas[i]));
+            float radius = stod(line.substr(commas[0] + 2, end));
+            cout << "\nSquare = " << pi * pow(radius, 2)
+                << "\nPerimeter = " << pi * 2 * radius << "\n";
         }
 
-        begin = commas[i] + 1;
+        delete commas;
     }
 
-    for(int i = commas[comma_count - 1] + 2; i < end; i++)
+    if(line.substr(0, 8) == "triangle")
+    {
+        for(int i = commas[comma_count - 1] + 2; i < end; i++)
         if(line[i] == ' ') space = i;
 
-    error += NumberCheck(line, commas[comma_count - 1] + 2, space);
-    error += NumberCheck(line, space + 1, end);
+        error += NumberCheck(line, commas[comma_count - 1] + 2, space);
+        error += NumberCheck(line, space + 1, end);
 
-    if(!error)
-    {
-        xy[comma_count][0] = stod(line.substr(commas[comma_count - 1] + 2, space));
-        xy[comma_count][1] = stod(line.substr(space + 1, end));
-
-        double perimeter = 0, square = 0;
-
-        for (int i = 0; i < comma_count + 1; i++)
+        if(!error)
         {
-            int j = (i + 1) % (comma_count + 1);
+            xy[comma_count][0] = stod(line.substr(commas[comma_count - 1] + 2, space));
+            xy[comma_count][1] = stod(line.substr(space + 1, end));
 
-            double dx = xy[i][0] - xy[j][0];
-            double dy = xy[i][1] - xy[j][1];
+            double a = sqrt(pow((xy[1][0] - xy[0][0]), 2) + pow((xy[1][1] - xy[0][1]), 2));
+            double b = sqrt(pow((xy[2][0] - xy[1][0]), 2) + pow((xy[2][1] - xy[1][1]), 2));
+            double c = sqrt(pow((xy[0][0] - xy[2][0]), 2) + pow((xy[0][1] - xy[2][1]), 2));
 
-            perimeter += sqrt(pow(dx, 2) + pow(dy, 2));
-            square += xy[i][0] * xy[j][1] - xy[j][0] * xy[i][1];
+            double p = a + b + c;
+
+            cout << "\nSquare = " << sqrt(p / 2 * (p / 2 - a) * (p / 2 - b) * (p / 2 - c))
+                << "\nPerimeter = " << p << "\n";
         }
+    }
 
-        cout << "\nSquare = " << fabs(square / 2)
-             << "\nPerimeter = " << perimeter << "\n";
+    if(line.substr(0, 7) == "polygon")
+    {
+        for(int i = commas[comma_count - 1] + 2; i < end; i++)
+        if(line[i] == ' ') space = i;
+
+        error += NumberCheck(line, commas[comma_count - 1] + 2, space);
+        error += NumberCheck(line, space + 1, end);
+
+        if(!error)
+        {
+            xy[comma_count][0] = stod(line.substr(commas[comma_count - 1] + 2, space));
+            xy[comma_count][1] = stod(line.substr(space + 1, end));
+
+            double perimeter = 0, square = 0;
+
+            for (int i = 0; i < comma_count + 1; i++)
+            {
+                int j = (i + 1) % (comma_count + 1);
+
+                double dx = xy[i][0] - xy[j][0];
+                double dy = xy[i][1] - xy[j][1];
+
+                perimeter += sqrt(pow(dx, 2) + pow(dy, 2));
+                square += xy[i][0] * xy[j][1] - xy[j][0] * xy[i][1];
+            }
+
+            cout << "\nSquare = " << fabs(square / 2)
+                << "\nPerimeter = " << perimeter << "\n";
+        }
     }
 
     delete commas;
@@ -252,14 +224,7 @@ void Lexer(int lines_count, string *lines)
 
         int *commas = CommaCount(lines[i], comma_count, open_bracket, close_bracket);
 
-        if(lines[i].substr(0, open_bracket) == "circle")
-            CircleCheck(lines[i], commas, open_bracket, close_bracket, error);
-
-        if(lines[i].substr(0, open_bracket) == "triangle")
-            TriangleCheck(lines[i], commas, comma_count, open_bracket, close_bracket, error);
-
-        if(lines[i].substr(0, open_bracket) == "polygon")
-            PolygonCheck(lines[i], commas, comma_count, open_bracket, close_bracket, error);
+        FigureCheck(lines[i], commas, comma_count, open_bracket, close_bracket, error);
     }
 
     delete lines;
